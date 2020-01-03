142a94e5d97dee997b049d31d70bcb673124b93f
lalalazero <zzzero520@hotmail.com>
2020-01-03 13:41:10 +0800

    build: add webpack-bundle-analyzer; add scripts

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,6 @@
 const path = require("path");
 const example = process.env.EXAMPLE_ENV;
+const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
 module.exports = {
   publicPath: example === "true" ? "/zeroview/" : "/",
   runtimeCompiler: true, // 使用包含运行时+编译器的vue构建版本,
@@ -8,6 +9,21 @@ module.exports = {
       preProcessor: "scss",
       patterns: [path.resolve(__dirname, "./src/style.scss")]
     }
+  },
+  configureWebpack: {
+    // 这样写没用，所以要改成 webpack 构建
+    // entry: {
+    //   app: example === 'true' ? './src/example.js' : './src/index.js' ,
+    //   vendor: ['vue']
+    // },
+    plugins: [
+      new BundleAnalyzerPlugin({
+        // default config
+        // analyzerMode: 'server',
+        // analyzerHost: '127.0.0.1',
+        // analyzerPort: 8888
+      })
+    ]
   },
   chainWebpack: config => {
     config.module
