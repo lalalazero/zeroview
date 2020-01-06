5a37a42941a0115ecfe83ec2d4aabda56b9af7e1
lalalazero <zzzero520@hotmail.com>
2020-01-06 18:06:42 +0800

    build: update circleci config

--- deploy_ghpages.sh
+++ deploy_ghpages.sh
@@ -7,18 +7,18 @@ set -e
 # show where we are on the machine
 pwd
 
-cp -rf ./dist ../dist
+cd dist
 
-git checkout gh-pages
-git rm -rf .
-
-cp -rf ../dist .
-
+git init
 git add -A
 git commit --allow-empty -m 'deploy to github pages [circleci]'
-# send any output to /dev/null to hide anything sensitive
-git push --force --quiet origin gh-pages > /dev/null 2>&1
+git checkout gh-pages
 
-rm -rf ../dist
+git remote add origin https://github.com/lalalazero/zeroview
+# send any output to /dev/null to hide anything sensitive
+git push -u origin gh-pages > /dev/null 2>&1
+
+cd ..
+rm -rf dist
 
 echo 'end deploy ------------------'
