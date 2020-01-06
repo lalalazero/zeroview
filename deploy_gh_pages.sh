5712558b142bed14ad7e1514c0409ce5c21dea34
lalalazero <zzzero520@hotmail.com>
2020-01-06 20:13:31 +0800

    build: update .sh

--- deploy_gh_pages.sh
+++ deploy_gh_pages.sh
@@ -2,7 +2,7 @@
 # ideas used from https://gist.github.com/motemen/8595451
 
 # abort the script if there is a non-zero error
-set -e
+# set -e
 
 # show where we are on the machine
 pwd
@@ -10,6 +10,8 @@ pwd
 remote=$(git config remote.origin.url)
 
 siteSource="$1"
+
+echo 'hello'
 
 echo $siteSource
 
@@ -29,15 +31,15 @@ echo 'enter ..'
 
 pwd
 # now lets setup a new repo so we can update the gh-pages branch
-git config --global user.email "$GH_EMAIL" > /dev/null 2>&1
-git config --global user.name "$GH_NAME" > /dev/null 2>&1
+git config --global user.email "$GH_EMAIL"
+git config --global user.name "$GH_NAME"
 git init
 git remote add --fetch origin "$remote"
 
 echo $remote
 
 # switch into the gh-pages branch
-if git rev-parse --verify origin/gh-pages > /dev/null 2>&1
+if git rev-parse --verify origin/gh-pages
 then
     git checkout gh-pages
     # delete any old site as we are going to replace it
@@ -57,7 +59,7 @@ git add -A
 # now commit, ignoring branch gh-pages doesn't seem to work, so trying skip
 git commit --allow-empty -m "Deploy to GitHub pages [ci skip]"
 # and push, but send any output to /dev/null to hide anything sensitive
-git push --force --quiet origin gh-pages > /dev/null 2>&1
+git push --force --quiet origin gh-pages
 
 # go back to where we started and remove the gh-pages git repo we made and used
 # for deployment
