10ca16bdb673c96f157340f8303de1a3436768ec
lalalazero <zzzero520@hotmail.com>
2020-01-07 14:40:12 +0800

    build: update .sh ; config.yml

--- deploy.sh
+++ deploy.sh
@@ -10,7 +10,16 @@ cp -a ./dist/. .
 git status
 
 git add .
+git add -A
+
+echo '不应该有没有 staged 的文件 ==============='
+git status
+
+echo 'commit ==============='
+
 git commit -m 'deploy'
+
+echo 'push ==============='
 
 git push
 
