b5c00cef99278ab995c0ebd0cc7fd0aac7fe66f7
lalalazero <zzzero520@hotmail.com>
2020-01-07 13:56:57 +0800

    build: update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -4,4 +4,25 @@ echo 'deploy'
 
 git checkout gh-pages
 
+if [ -d 'dist' ]
+then
+  echo 'dist is empty'
+  git checkout master
+  exit 1
+fi
 
+echo 'checkout master'
+git checkout master
+
+exit 0
+
+cp -a dist .
+
+git status
+
+git add -A
+git commit -m 'deploy'
+
+git push
+
+git checkout master
