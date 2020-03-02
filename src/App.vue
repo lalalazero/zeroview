9c519c09293660ddc133116728a0583e844326ce
lalalazero <zzzero520@hotmail.com>
2020-03-02 22:07:58 +0800

    初步完成 uploader, 上传一个文件并成功

--- src/App.vue
+++ src/App.vue
@@ -1,112 +1,18 @@
 <template>
   <div class="app">
-    <z-view-upload>upload</z-view-upload>
+    <z-view-upload accept="image/**" action="http://localhost:3000/asubmit" name="file">
+      <button>上传</button>
+      <template slot="tips">
+        <div>只能上传 300kb 以内的 png,jpeg 文件</div>
+      </template>
+    </z-view-upload>
+    <button>保存</button>
   </div>
 </template>
 
 
 <script>
-export default {
-  components: {},
-  name: "App",
-  mounted() {},
-  methods: {
-    edit(item) {
-      alert(`编辑了id=${item.id}`);
-    },
-    view(item) {
-      alert(`查看了id=${item.id}`);
-    }
-  },
-  data() {
-    return {
-      item: undefined,
-      columns: [
-        {
-          index: "name",
-          text: "姓名",
-          width: 200
-        },
-        {
-          index: "score",
-          text: "分数"
-        },
-        {
-          index: "addr",
-          text: "住址"
-        }
-      ],
-      dataSource: [
-        {
-          id: 1,
-          name: "张三",
-          score: 85,
-          desc: "张三是个男孩子",
-          addr: "中国四川成都市天府新区麓山大道"
-        },
-        {
-          id: 2,
-          name: "李四",
-          score: 99,
-          desc: "张三是个男孩子",
-          addr: "中国四川成都市天府新区麓山大道"
-        },
-        {
-          id: 3,
-          name: "王麻子",
-          score: 100,
-          desc: "张三是个男孩子",
-          addr: "中国四川成都市天府新区麓山大道"
-        },
-        {
-          id: 4,
-          name: "狗蛋",
-          score: 72,
-          desc: "张三是个男孩子",
-          addr: "中国四川成都市天府新区麓山大道"
-        },
-        {
-          id: 5,
-          name: "翠花",
-          score: 88,
-          desc: "张三是个男孩子",
-          addr: "中国四川成都市天府新区麓山大道"
-        },
-        {
-          id: 6,
-          name: "丫丫",
-          score: 60,
-          desc: "张三是个男孩子",
-          addr: "中国四川成都市天府新区麓山大道"
-        },
-        {
-          id: 7,
-          name: "翠花",
-          score: 88,
-          addr: "中国四川成都市天府新区麓山大道"
-        },
-        {
-          id: 8,
-          name: "翠花",
-          score: 88,
-          addr: "中国四川成都市天府新区麓山大道"
-        },
-        {
-          id: 9,
-          name: "翠花",
-          score: 88,
-          addr: "中国四川成都市天府新区麓山大道"
-        },
-        {
-          id: 10,
-          name: "翠花",
-          score: 88,
-          addr: "中国四川成都市天府新区麓山大道"
-        }
-      ],
-      selectedItems: []
-    };
-  }
+export default{
 };
 </script>
 
--- src/components/upload/upload.vue
+++ src/components/upload/upload.vue
@@ -1,11 +1,49 @@
 <template>
   <div class="z-view-upload-wrapper">
-    <slot></slot>
+    <div @click="onClickUpload" ref="trigger">
+      <slot></slot>
+    </div>
+    <div ref="temp" style="width: 0;height: 0;overflow: hidden"></div>
+    <slot name="tips"></slot>
   </div>
 </template>
 <script>
 export default {
-  name: "zViewUpload"
+  name: "zViewUpload",
+  props: {
+    name: {
+      type: String,
+      required: true
+    },
+    method: {
+      type: String,
+      default: 'POST'
+    },
+    action: {
+      type: String
+    }
+  },
+  methods: {
+    onClickUpload(){
+      let input = document.createElement('input')
+      input.type = 'file'
+      this.$refs.temp.appendChild(input)
+      input.addEventListener('change',()=>{
+        console.log(input.files)
+        let file = input.files[0]
+        input.remove()
+        let formData = new FormData()
+        formData.append(this.name, file)
+        let xhr = new XMLHttpRequest()
+        xhr.open(this.method, this.action)
+        xhr.onload = () => {
+          console.log(xhr.response)
+        }
+        xhr.send(formData)
+      })
+      input.click()
+    }
+  }
 };
 </script>
 <style lang="scss" scoped>
