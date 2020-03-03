cff73dd238c5ea3f878559d5393d9bc1865bc075
lalalazero <zzzero520@hotmail.com>
2020-03-03 21:51:45 +0800

    feat(upload.vue): 显示上传中

--- src/components/upload/upload.vue
+++ src/components/upload/upload.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="z-view-upload-wrapper">
+  <div class="z-view-uploader">
     <div @click="onClickUpload" ref="trigger">
       <slot></slot>
     </div>
@@ -9,6 +9,9 @@
       <ol>
         <li v-for="(file,index) in fileList" :key="`${index}-${file.name}`">
           {{ file.name }}
+          <span v-if="file.status === 'uploading'" class="z-view-uploader-uploading">
+            <z-view-icon name="loading"></z-view-icon>
+          </span>
           <img :src="file.url" style="width: 40px; height: 40px"/>
           <button @click="onRemoveFile(file)">x</button>
         </li>
@@ -17,9 +20,71 @@
   </div>
 </template>
 <script>
-import input from "../input/input";
-
+import zViewIcon from '../icon/icon.vue'
 export default {
+  components: {
+    zViewIcon
+  },
+  methods: {
+    onClickUpload(){
+      let input = this.createInput()
+      // listen to input
+      input.addEventListener('change',()=>{
+        let file = input.files[0]
+        this.updateFile(file)
+        input.remove()
+      })
+      input.click()
+    },
+    createInput(){
+      let input = document.createElement('input')
+      input.type = 'file'
+      this.$refs.temp.appendChild(input)
+      return input
+    },
+    beforeUploadFile(file, uid){
+      let { name, size, type } = file
+      this.$emit('update:fileList',[...this.fileList, { uid, name, type, size, status:'uploading'}])
+    },
+    afterUploadFile(rawFile, url, uid){
+      let file = this.fileList.find(file => file.uid === uid)
+      let copy = JSON.parse(JSON.stringify(file))
+      copy.status = 'success'
+      copy.url = url
+      console.log(copy)
+      let index = this.fileList.indexOf(file)
+      this.fileList.splice(index, 1, copy)
+      let copyList = [...this.fileList]
+      this.$emit('update:fileList',copyList)
+    },
+    updateFile(rawFile){
+      let uid = this.uid++
+      this.beforeUploadFile(rawFile, uid)
+      let formData = new FormData()
+      formData.append(this.name, rawFile)
+      this.doUploadFile(formData,(response)=>{
+        let url = this.parseResponse(response)
+        this.afterUploadFile(rawFile, url, uid)
+      })
+    },
+    doUploadFile(formData, success){
+      let xhr = new XMLHttpRequest()
+      xhr.open(this.method, this.action)
+      xhr.onload = ()=>{
+        success(xhr.response)
+      }
+      xhr.send(formData)
+    },
+    onRemoveFile(file){
+      let answer = window.confirm('确定删除吗？')
+      if(answer){
+        let copy = [...this.fileList]
+        let index = copy.indexOf(file)
+        copy.splice(index, 1)
+        this.$emit('update:fileList',copy)
+      }
+    }
+  },
   name: "zViewUpload",
   props: {
     name: {
@@ -43,57 +108,19 @@ export default {
   },
   data(){
     return {
-      url: 'about:blank'
-    }
-  },
-  methods: {
-    onClickUpload(){
-      let input = this.createInput()
-      // listen to input
-      input.addEventListener('change',()=>{
-        let file = input.files[0]
-        this.updateFile(file)
-        input.remove()
-      })
-      input.click()
-    },
-    createInput(){
-      let input = document.createElement('input')
-      input.type = 'file'
-      this.$refs.temp.appendChild(input)
-      return input
-    },
-    updateFile(file){
-      let formData = new FormData()
-      formData.append(this.name, file)
-      let { name, size, type } = file
-      this.doUploadFile(formData,(response)=>{
-        let url = this.parseResponse(response)
-        this.$emit('update:fileList', [...this.fileList, { name, type, size, url }])
-      })
-    },
-    doUploadFile(formData, success){
-      let xhr = new XMLHttpRequest()
-      xhr.open(this.method, this.action)
-      xhr.onload = ()=>{
-        success(xhr.response)
-      }
-      xhr.send(formData)
-    },
-    onRemoveFile(file){
-      let answer = window.confirm('确定删除吗？')
-      if(answer){
-        let copy = [...this.fileList]
-        let index = copy.indexOf(file)
-        copy.splice(index, 1)
-        this.$emit('update:fileList',copy)
-      }
+      url: 'about:blank',
+      uid: 1,
     }
   }
 };
 </script>
 <style lang="scss" scoped>
-  .z-view-upload-wrapper {
+  .z-view-uploader {
     border: 1px solid #000;
+    &-uploading {
+      > svg {
+        animation: $spinAnimation;
+      }
+    }
   }
 </style>
