d849857612187f8f0950c748e50788985eb9d59c
lalalazero <zzzero520@hotmail.com>
2020-03-18 18:09:14 +0800

    feat:(datepicker.vue) 初步完成日期面板的样式

--- src/components/date-picker/date-picker.vue
+++ src/components/date-picker/date-picker.vue
@@ -6,9 +6,23 @@
         <Icon name="prev-double" /><Icon name="left" /><span @click="onClickYear">2020年</span><span @click="onClickMonth">8月</span><Icon name="right" /><Icon name="next-double" />
       </div>
       <div class="z-view-date-picker-content-wrapper">
-        <div v-if="mode === 'day'" class="z-view-date-picker-content-days">日</div>
-        <div v-if="mode === 'month'" class="z-view-date-picker-content-months">月</div>
-        <div v-if="mode === 'year'" class="z-view-date-picker-content-years">年</div>
+        <div v-if="mode === 'day'" class="z-view-date-picker-content-days-panel">
+          <ul class="z-view-date-picker-content-days-panel-weekends">
+            <li v-for="weekday in 7" :key="weekday" class="z-view-date-picker-weekday-cell">
+              {{ WEEKDAYS[weekday - 1] }}
+            </li>
+          </ul>
+          <ul v-if="days.length > 0" class="z-view-date-picker-content-days-panel-days">
+            <li v-for="week in 6" :key="`${week}`">
+                <span v-for="day in 7" :key="`${week}-${day}`" class="z-view-date-picker-cell" @click="clickDateCell(days[(day + 7 * (week - 1)) - 1])"
+                 :class="classes(days[(day + 7 * (week - 1)) - 1])">
+                  <span class="z-view-date-picker-day-cell">{{ days[(day + 7 * (week - 1)) - 1].getDate() }}</span>
+                </span>
+            </li>
+          </ul>
+        </div>
+        <div v-if="mode === 'month'" class="z-view-date-picker-content-months-panel">月</div>
+        <div v-if="mode === 'year'" class="z-view-date-picker-content-years-panel">年</div>
       </div>
       <div class="z-view-date-picker-footer"></div>
     </div>
@@ -18,7 +32,7 @@
 import Input from '../input/input.vue'
 import Icon from '../icon/icon.vue'
 import ClickOutSide from '@/directives/click-outside.js'
-import { firstDayOfMonth, lastDayOfMonth } from './helper'
+import { firstDayOfMonth, lastDayOfMonth, getYearMonthDate, WEEKDAYS } from './helper'
 export default {
   name: 'zViewDatePicker',
   directives: { ClickOutSide },
@@ -30,8 +44,15 @@ export default {
     return {
       popVisible: false,
       mode: 'day',
-      value: new Date()
+      date: new Date(),
+      days: [],
+      WEEKDAYS,
+      today: new Date(),
+      selected: new Date()
     }
+  },
+  computed: {
+
   },
   methods: {
     onFocus(){
@@ -46,18 +67,93 @@ export default {
     onClickYear(){
       this.mode = 'year'
     },
-    getFullYearDate(year){
-      for(let i = 0; i <= 11; i++){
-        let date = new Date(year,i,10)
-        let firstDay = firstDayOfMonth(date)
-        console.log(firstDay.toLocaleDateString())
-        let lastDay = lastDayOfMonth(date)
-        console.log(lastDay.toLocaleDateString())
+    getCurrentMonthDays(){
+      let currentMonthDays = []
+      let { year, month, day } = getYearMonthDate(this.date)
+      let firstDay = firstDayOfMonth(this.date)
+      let lastDay = lastDayOfMonth(this.date)
+      let firstDayDate = firstDay.getDate()
+      let lastDayDate = lastDay.getDate()
+      for(let i = firstDayDate; i <= lastDayDate; i++){
+        currentMonthDays.push(new Date(year, month, i))
       }
+      return currentMonthDays
+    },
+    getPreviousMonthDays(){
+      // 0 - Sunday
+      let firstDay = firstDayOfMonth(this.date)
+      let weekDay = firstDay.getDay()
+      // 从星期一开始
+      let len = 0
+      if(weekDay === 0){ // 第一天是周日，往前补6天
+        len = 6
+      }else if(weekDay === 1){ // 第一天是周一，往前补一个星期
+        len = 7
+      }else{
+        len = weekDay - 1
+      }
+
+      let prevMonthDays = []
+      let temp = new Date(this.date.getTime())
+      temp.setDate(0)
+      let lastDayOfLastMonth = new Date(temp.getTime())
+      let { year, month, day } = getYearMonthDate(lastDayOfLastMonth)
+
+      for(let i = 0; i < len; i++){
+        prevMonthDays.push(new Date(year, month, day - i))
+      }
+      return prevMonthDays.reverse()
+    },
+    getNextMonthDays(len){
+      let firstDay = firstDayOfMonth(this.date)
+      let { year, month, day} =  getYearMonthDate(firstDay)
+      let nextMonthDays = []
+      for(let i = 0; i < len; i++){
+        nextMonthDays.push(new Date(year, month + 1, day + i))
+      }
+      return nextMonthDays
+    },
+    getDays(){
+      let currentMonthDays = this.getCurrentMonthDays()
+      let previousMonthDays = this.getPreviousMonthDays()
+      let len = currentMonthDays.length + previousMonthDays.length
+      let nextMonthDays = this.getNextMonthDays(42 - len)
+      return [...previousMonthDays,...currentMonthDays, ...nextMonthDays]
+    },
+    isCurrentMonth(date){
+      if(!(date instanceof Date)) return
+      let { year, month, day } = getYearMonthDate(date)
+      return month === this.date.getMonth()
+    },
+    isToday(date){
+      if(!(date instanceof Date)) return
+      let { year, month, day } = getYearMonthDate(date)
+      let { year: year2, month: month2, day: day2 } = getYearMonthDate(this.today)
+      return year === year2 && month === month2 && day === day2
+    },
+    isSelected(date){
+      if(!(date instanceof Date)) return
+      if(!this.selected) return false
+      let { year, month, day } = getYearMonthDate(date)
+      let { year: year2, month: month2, day: day2 } = getYearMonthDate(this.selected)
+      return year === year2 && month === month2 && day === day2
+    },
+    classes(date){
+      return {
+        'z-view-date-picker-cell-inview': this.isCurrentMonth(date),
+        'z-view-date-picker-cell-today': this.isToday(date),
+        'z-view-date-picker-cell-selected': this.isSelected(date),
+      }
+    },
+    clickDateCell(date){
+      if(!(date instanceof Date)) return
+      this.selected = date
+      this.popVisible = false
     }
   },
   mounted(){
-    this.getFullYearDate(2019)
+    this.days = this.getDays()
+    console.log(this.days)
   },
 }
 </script>
@@ -66,11 +162,67 @@ export default {
   display: inline-block;
   border: 1px solid red;
   &-panel {
-    width: 188px;
+    padding: 5px 10px;
+    width: 280px;
     position: absolute;
     background: #fff;
     z-index: 3;
     border: 1px solid #000;
   }
+  &-content-days-panel {
+    &-weekends {
+      display: flex;
+      justify-content: space-evenly;
+    }
+    &-days {
+      li {
+        display: flex;
+        justify-content: space-evenly;
+        margin: 2px 0;
+      }
+    }
+  }
+  &-weekday-cell {
+    width: 36px;
+    height: 30px;
+  }
+  &-day-cell {
+    width: 24px;
+    height: 24px;
+    color: rgba(0, 0, 0, 0.25);
+    &:hover {
+      background: #f5f5f5;
+    }
+  }
+  &-weekday-cell,&-cell,&-day-cell {
+    display: inline-flex;
+    justify-content: center;
+    align-items: center;
+  }
+  &-cell {
+    padding: 3px 0;
+    width: 36px;
+    cursor: pointer;
+    color: rgba(0, 0, 0, 0.25);
+    &-inview {
+      > span {
+        color: rgba(0, 0, 0, 0.65);
+      }
+
+    }
+    &-today {
+      .z-view-date-picker-day-cell {
+        border: 1px solid $--primary-color;
+      }
+    }
+    &-selected{
+      .z-view-date-picker-day-cell {
+        background: $--primary-color;
+        border: none;
+        color: #fff;
+      }
+    }
+  }
+
 }
 </style>
--- src/components/date-picker/helper.js
+++ src/components/date-picker/helper.js
@@ -1,21 +1,26 @@
 const firstDayOfMonth = date => {
-  let [year, month, day] = getYearMonthDate(date)
+  let { year, month, day} = getYearMonthDate(date)
   return new Date(year, month, 1)
 }
 const lastDayOfMonth = date => {
   let temp = new Date(date.getTime())
   temp.setDate(1)
-  let [year, month, day] = getYearMonthDate(temp)
+  let { year, month, day } = getYearMonthDate(temp)
   return new Date(year, month + 1, 0)
 }
-export {
-  firstDayOfMonth,
-  lastDayOfMonth
-}
-function getYearMonthDate(date){
+
+const getYearMonthDate = date => {
   let year = date.getFullYear()
   let month = date.getMonth()
   let day = date.getDate()
-  return [year, month, day]
+  return { year, month, day }
+}
 
+const WEEKDAYS = ['一','二','三','四','五','六','日']
+
+export {
+  firstDayOfMonth,
+  lastDayOfMonth,
+  getYearMonthDate,
+  WEEKDAYS
 }
